75c0a52c3ff61651e88225ea3ae0c8fa5c2b52f5
Choudhury <archoudhury@teksystems.com>
2022-01-27 01:13:56 +0530

    fetch from contract

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+  const [gretting, setGrettingValue] = useState();
+
+  async function requestAccount() {
+    await window.ethereum.request({method: 'eth_requestAccounts'});
+  }
+
+  async function fetchGreeting() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", Greeter.abi, provider);
+
+      try {
+        const data = await contract.greet();
+        setGrettingValue(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if(gretting) {
+      if (window.ethereum) {
+        await requestAccount();
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(ethers.constants.AddressZero, Greeter.abi, signer);
+
+        const transaction = await contract.setGreeting(gretting);
+        await transaction.wait();
+        
+        await fetchGreeting();
+      }
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={requestAccount}>Request account</button>
+        <button onClick={fetchGreeting}>fetchGreeting</button>
+        <button onClick={setGreeting}>setGreeting</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {gretting}
         </p>
         <a
           className="App-link"
